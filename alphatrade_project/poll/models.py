157ee443434fcccedd4fb50dfab8c9ca0934843b
dantasraf <40270061+dantasraf@users.noreply.github.com>
2020-11-21 16:37:56 -0300

    Correção de erros na votação e limpeza do código

--- alphatrade_project/poll/models.py
+++ alphatrade_project/poll/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-""" class Poll(models.Model):
-    question = models.TextField()
-    option_one = models.CharField(max_length=30)
-    option_two = models.CharField(max_length=30)
-    option_three = models.CharField(max_length=30)
-	#option_four = models.CharField(max_length=30)
-    option_one_count = models.IntegerField(default=0)
-    option_two_count = models.IntegerField(default=0)
-    option_three_count = models.IntegerField(default=0)
-	#option_four_count = models.IntegerField(default=0)
-
-    def total(self):
-        return self.option_one_count + self.option_two_count + self.option_three_count #+ self.option_four_count """
 
 class MyPoll(models.Model):
     question = models.TextField()
@@ -30,50 +17,3 @@ class MyPoll(models.Model):
 
     def total(self):
         return self.option_one_count + self.option_two_count + self.option_three_count + self.option_four_count
-""" class Customer(models.Model):
-	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-	def __str__(self):
-		return self.name
-
-
-class Tag(models.Model):
-	name = models.CharField(max_length=200, null=True)
-
-	def __str__(self):
-		return self.name
-
-class Product(models.Model):
-	CATEGORY = (
-			('Indoor', 'Indoor'),
-			('Out Door', 'Out Door'),
-			) 
-
-	name = models.CharField(max_length=200, null=True)
-	price = models.FloatField(null=True)
-	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True, blank=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	tags = models.ManyToManyField(Tag)
-
-	def __str__(self):
-		return self.name
-
-class Order(models.Model):
-	STATUS = (
-			('Pending', 'Pending'),
-			('Out for delivery', 'Out for delivery'),
-			('Delivered', 'Delivered'),
-			)
-
-	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
-	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	note = models.CharField(max_length=1000, null=True)
-
-	def __str__(self):
-		return self.product.name """
